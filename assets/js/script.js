8412ac75f4ebf8d55b8ba52b0e80fb592fc084f8
Justin <dong135790@gmail.com>
2023-05-18 16:19:12 -0400

    document change.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
 
   // Current day and logs it into header based on id=currentDay.
   var day = dayjs();
